2c254f09b391814cb225982c5e95e1691c27325d
Gabriel <gfreundt@gmail.com>
2025-05-18 20:56:37 -0500

    .

--- src/updates/gather_recvehic.py
+++ src/updates/gather_recvehic.py
@@ -86,13 +86,13 @@ def gather(db_cursor, db_conn, dash, update_data):
             except KeyboardInterrupt:
                 quit()
 
-            except:
-                retry_attempts += 1
-                dash.log(
-                    card=CARD,
-                    status=2,
-                    text=f"|ADVERTENCIA| Reintentando [{retry_attempts}/3]: {doc_tipo} {doc_num}",
-                )
+            # except:
+            #     retry_attempts += 1
+            #     dash.log(
+            #         card=CARD,
+            #         status=2,
+            #         text=f"|ADVERTENCIA| Reintentando [{retry_attempts}/3]: {doc_tipo} {doc_num}",
+            #     )
 
         db_conn.commit()
 
