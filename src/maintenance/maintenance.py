5471e437fca1efee5d07793873c602a6ae2f0b23
Gabriel <gfreundt@gmail.com>
2025-05-25 21:12:01 -0500

    email block 1 success

--- src/maintenance/maintenance.py
+++ src/maintenance/maintenance.py
@@ -4,17 +4,22 @@ import os
 def pre_maint(db_cursor):
 
     # erase all comms that might still be in outbound folder
-    # for file in os.listdir("outbound"):
-    #     if "alert" in file or "message" in file:
-    #         os.remove(os.path.join("outbound", file))
-
-    # erase all placas with no associated member for database
-    cmd = "SELECT * FROM placas WHERE IdMember_FK NOT IN (SELECT IdMember FROM members)"
-    db_cursor.execute(cmd)
-    print(db_cursor.fetchall())
+    for file in os.listdir("outbound"):
+        if "alert" in file or "message" in file:
+            os.remove(os.path.join("outbound", file))
 
 
 def post_maint(db_cursor):
     # extract text data from soat images
-    # create reviews: duplicate placas
-    pass
+
+    # review: duplicate placas
+    cmd = """     DELETE FROM '$review';
+                    INSERT INTO '$review' 
+                        SELECT NULL, Placa, "Placa Duplicada", NULL FROM placas GROUP BY Placa HAVING COUNT(*) > 1;
+            """
+    db_cursor.execute(cmd)
+
+    # review: placas with no associated member
+    cmd = """   INSERT INTO '$review' SELECT NULL, Placa, "Placa sin Usuario", NULL FROM placas
+                WHERE IdMember_FK NOT IN (SELECT IdMember FROM members)"""
+    db_cursor.execute(cmd)
