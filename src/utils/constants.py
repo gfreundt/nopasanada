3b4f2f73bb34c067117dc35ccdd917b5468bbcca
Gabriel <gfreundt@gmail.com>
2025-07-07 17:58:14 -0500

    .

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -4,11 +4,10 @@ import platform
 # paths
 if platform.system() == "Linux":
     NETWORK_PATH = os.path.join("/mnt/nopasanada_server")
-else:
+elif platform.system() == "Windows":
     NETWORK_PATH = os.path.join(r"\\192.168.68.110", "d", "pythonCode", "nopasanada")
 
 DB_NETWORK_PATH = os.path.join(NETWORK_PATH, "data", "members.db")
-print(DB_NETWORK_PATH, "++++++++++++++++++++++++++++++++")
 DB_LOCAL_PATH = os.path.join("data", "members.db")
 
 # info email account
